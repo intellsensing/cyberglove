f20ce7fdb68ba74094ee741aef74590a115d268e
agamemnonc <agamemnon.krasoulis@gmail.com>
2019-05-16 12:44:06 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+""" Setup script for cyberglove package. """
+
+from setuptools import setup
+
+setup(
+    name = "cyberglove",
+    version = "0.0.1",
+    author = "Agamemnon Krasoulis",
+    description = ("Interface the CyberGlove Systems data glove in Python.")
+    url = "https://github.com/agamemnonc/cyberglove",
+    packages=['cyberglove']
+)
